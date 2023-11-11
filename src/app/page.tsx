4b53cd7db133292a407e9b8aab992d4fa3fc9d1d
elhadjcire224 <bahmamadoucire14@gmail.com>
2023-11-11 09:34:16 +0000

    ajout des composants shadcn-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
    <div className='bg-red-500'>
      hello les devs
+     <div
+  className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
+/>
    </div>
   )
 }
