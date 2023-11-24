3f2559422bfe318bd6cff8da7094ac22d98cbb0a
elhadjcire224 <bahmamadoucire14@gmail.com>
2023-11-24 12:47:28 +0000

    odif user interface

--- src/lib/nextauth.d.ts
+++ src/lib/nextauth.d.ts
@@ -1,5 +1,5 @@
 import { Role } from "@prisma/client";
-import type { DefaultSession,User } from "next-auth";
+import type { DefaultSession,DefaultUser } from "next-auth";
 
 declare module 'next-auth'{
     
@@ -12,7 +12,7 @@ declare module 'next-auth'{
         }
     }
 
-    interface User{
+    interface User extends DefaultUser {
         role?:Role
         subject?:any
     }
